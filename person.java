1d4968a39dff92d87fb0be2d78813ebedf26484c
code-with-gayu <gv1619022@gmail.com>
2025-07-21 22:42:13 -0700

    Add files via upload

---
+++ person.java
@@ -0,0 +1,14 @@
+public class person {
+    
+      public static void main(String[] args) {
+        
+        String name = "gayathri"                   ;
+        double weight=155.5;
+        double height=167.9;
+
+        System.out.println("person name: "+name);
+        System.out.println("person weight:"+weight);
+        System.out.println("person height:"+weight);
+
+      }
+}

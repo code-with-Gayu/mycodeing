1e2b15ec14f8f257b3f3d814bc0a4da0776895c4
code-with-gayu <gv1619022@gmail.com>
2025-07-21 22:38:00 -0700

    Add files via upload

---
+++ code2.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+public class code2 {
+    public static void main(String[]args)
+    {
+Scanner mydata=new Scanner(System.in);
+
+   System.out.println("enter the number");
+   int num=mydata.nextInt();
+
+   if(num%2==0)
+   {
+    System.out.println("the number is even");
+
+   }
+    else
+     {
+        System.out.println("the number is odd");
+    }
+   }
+    }
+
